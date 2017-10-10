743d4b3e284cdfae7301d9be5ef91b8444f6f01b
Misty Malkasian <andrewandmisty@gmail.com>
2017-10-10 15:38:43 -0500

    Created necessary classes

---
+++ Milk Stand/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milk_Stand
+{
+
+    class Customer
+    {
+        //member methods
+
+        //constructor
+
+        //member variables
+    }
+}
